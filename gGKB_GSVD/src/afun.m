9a1f94c09a9242f2b9ec7652355d9026d593756c
Haibo Li <lee12358@163.com>
2024-10-05 20:56:11 -0400

    Add files via upload

---
+++ gGKB_GSVD/src/afun.m
@@ -0,0 +1,16 @@
+function y = afun(z,A,B,transp_flag)
+% function handle to compute [A;B]*z or [A;B]'*z
+
+if strcmp(transp_flag,'transp')   % y = (A(I_n-BB^T))' * z;
+    m = size(A,1);
+    p = size(B,1);
+    s = A'*z(1:m);
+    t = B'*z(m+1:m+p);
+    y = s + t;
+elseif strcmp(transp_flag,'notransp') % y = (A(I_n-BB^T)) * z;
+    s = A*z;
+    t= B*z;
+    y = [s;t];
+    
+end
+end
